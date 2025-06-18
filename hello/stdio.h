513b6a042d0b54e1bf477f20430cea65d902f046
renan-calusa <renansscalusa@usp.br>
2025-06-18 20:06:12 -0300

    Setup

---
+++ hello/stdio.h
@@ -0,0 +1,16 @@
+/*
+ *    SPDX-FileCopyrightText: 2025 renan-calusa <renansscalusa@usp.br>
+ *   
+ *    SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ *  This file is a derivative of SYSeg (https://gitlab.com/monaco/syseg)
+ *  and includes modifications made by the following author(s):
+ *  renan-calusa <renansscalusa@usp.br>
+ */
+
+#ifndef E8_H
+#define E8_H
+
+void __attribute__((fastcall, naked)) printf(const char *); 
+
+#endif	
